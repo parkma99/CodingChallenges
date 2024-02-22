dd739869ee8998907a58ab6ed55c4bbec5545df3
parkma99 <park-ma@hotmail.com>
2024-02-22 23:38:03 +0800

    support multi host

--- PortScanner/ccscan/main.go
+++ PortScanner/ccscan/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -17,18 +18,34 @@ const MAX_PORT = 65536
 var timeout int
 var parallel_num int
 
-func cmd() (string, int, error) {
-	var host string
+type hostList []string
+
+func (hl *hostList) String() string {
+	return strings.Join(*hl, ",")
+}
+
+func (hl *hostList) Set(value string) error {
+	hosts := strings.Split(strings.TrimSpace(value), ",")
+	new_hl := make([]string, len(hosts))
+	for i, host := range hosts {
+		new_hl[i] = strings.TrimSpace(host)
+	}
+	*hl = new_hl
+	return nil
+}
+
+func cmd() (hostList, int, error) {
+	var hosts hostList
 	var port int
-	flag.StringVar(&host, "host", "", "host to connect")
+	flag.Var(&hosts, "host", "host to connect")
 	flag.IntVar(&port, "port", 0, "port to connect")
 	flag.IntVar(&timeout, "timeout", MAX_TIMEOUT_MS, "max timeout microsecond")
 	flag.IntVar(&parallel_num, "parallel", MAX_PARALLEL_NUM, "max parallel number")
 	flag.Parse()
-	if host == "" {
-		return "", port, errors.New("parse flag error")
+	if len(hosts) == 0 {
+		return []string{}, port, errors.New("parse flag error")
 	}
-	return host, port, nil
+	return hosts, port, nil
 }
 
 func tryCreateTCPConnect(host string, port int) {
@@ -40,38 +57,48 @@ func tryCreateTCPConnect(host string, port int) {
 	fmt.Printf("Port: %d is open\n", port)
 }
 func main() {
-	host, port, err := cmd()
+	hosts, port, err := cmd()
+	log.Println(hosts, port)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	if port != 0 {
-		fmt.Fprintf(os.Stdout, "Scanning host: %s port: %d\n", []any{host, port}...)
-		tryCreateTCPConnect(host, port)
-	} else {
-		fmt.Printf("Scanning host: %s \n", host)
-		low := 0
-		high := 0
-		step := MAX_PORT / parallel_num
-		done := make(chan bool)
-		for range parallel_num {
-			low = high + 1
-			if low > MAX_PORT {
-				break
-			}
-			high = low + step
-			if high >= MAX_PORT {
-				high = MAX_PORT
-			}
-			go func(low, high int) {
-				for i := low; i <= high; i++ {
-					tryCreateTCPConnect(host, i)
-				}
-				done <- true
-			}(low, high)
+		for _, host := range hosts {
+			fmt.Fprintf(os.Stdout, "Scanning host: %s port: %d\n", []any{host, port}...)
+			tryCreateTCPConnect(host, port)
 		}
-		for range parallel_num {
-			<-done
-		}
-	}
 
+	} else {
+		for _, host := range hosts {
+			parallel_scan(host)
+		}
+
+	}
+}
+
+func parallel_scan(host string) {
+	fmt.Printf("Scanning host: %s \n", host)
+	low := 0
+	high := 0
+	step := MAX_PORT / parallel_num
+	done := make(chan bool)
+	for range parallel_num {
+		low = high + 1
+		if low > MAX_PORT {
+			break
+		}
+		high = low + step
+		if high >= MAX_PORT {
+			high = MAX_PORT
+		}
+		go func(low, high int) {
+			for i := low; i <= high; i++ {
+				tryCreateTCPConnect(host, i)
+			}
+			done <- true
+		}(low, high)
+	}
+	for range parallel_num {
+		<-done
+	}
 }
